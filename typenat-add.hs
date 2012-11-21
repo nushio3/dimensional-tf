e651b7f66b692e98948ad1ed048c7697fa3e1fce
Takayuki Muranushi <muranushi@gmail.com>
2012-11-21 18:35:54 +0900

    try addition.

--- typenat-add.hs
+++ typenat-add.hs
@@ -3,10 +3,21 @@ KindSignatures, MultiParamTypeClasses, TypeOperators #-}
 
 import GHC.TypeLits
 
-type Six = (6 :: Nat)
-type Seven = (7 :: Nat)
-type FourtyTwo = (Six * Seven :: Nat)
+data Index (i :: Nat) = Get
+
+class Has a index b | a index -> b where
+  from :: a -> Index index -> b
+
+data Person = Person String String Int Int deriving Show
+
+instance Has Person 0 String where from (Person x _ _ _) _ = x
+instance Has Person 1 String where from (Person _ x _ _) _ = x
+instance Has Person 2 Int    where from (Person _ _ x _) _ = x
+instance Has Person 3 Int    where from (Person _ _ _ x) _ = x
+
+einstein = Person "Albert" "Einstein" 1879 1955
 
 main = do
-  print $ (sing :: Sing 42)
-  print $ (sing :: Sing FourtyTwo)
+  print $ from einstein (Get :: Index 0)
+  print $ from einstein (Get :: Index 1)
+  print $ from einstein (Get :: Index (1+1))
