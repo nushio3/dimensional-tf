ab6ad38d0914956e1095825dc34e867ae9f6e08f
Takayuki Muranushi <muranushi@gmail.com>
2012-11-24 15:07:42 +0900

    error message added.

--- attic/basic-02.hs
+++ attic/basic-02.hs
@@ -7,3 +7,16 @@ main = do
 --  print $ (sing :: Sing 6) * 7
   print $ fromSing (sing :: Sing 6) * 7
   print $ fromSing (sing :: Sing (6*7))
+
+{-
+$ runhaskell basic-02.hs
+
+basic-02.hs:9:21:
+    No instance for (SingI Nat (6 * 7)) arising from a use of `sing'
+    Possible fix: add an instance declaration for (SingI Nat (6 * 7))
+    In the first argument of `fromSing', namely
+      `(sing :: Sing (6 * 7))'
+    In the second argument of `($)', namely
+      `fromSing (sing :: Sing (6 * 7))'
+    In a stmt of a 'do' block: print $ fromSing (sing :: Sing (6 * 7))
+-}
