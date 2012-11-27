afde32b90933d349da49ca09021a885ea4e3c88c
Takayuki Muranushi <muranushi@gmail.com>
2012-11-27 11:03:56 +0900

    further shrink

--- unittyped/test-3.hs
+++ unittyped/test-3.hs
@@ -5,19 +5,13 @@ import UnitTyped.SI
 
 
 
-alpha =  1 gram * 1 meter 
-beta  =  1 gram * 1 meter * 1 second / 1 meter / 1 gram
+alpha =  1 gram 
+beta  =  1 gram * 1 second / 1 gram
 gamma =  1 second
 
 main = do
-  print $ alpha         -- 1.0 g⋅m      
-  print $ beta          -- 1.0 g⋅m⋅s/m/g
-  print $ gamma         -- 1.0 s        
-  print $ gamma + beta  -- 2.0 s        
-  print $ beta  + gamma -- 2.0 g⋅m⋅s/m/g
-
-
-
-
-
-
+  print $ alpha         -- 1.0 g    
+  print $ beta          -- 1.0 g⋅s/g
+  print $ gamma         -- 1.0 s    
+  print $ gamma + beta  -- 2.0 s    
+  print $ beta  + gamma -- 2.0 g⋅s/g
