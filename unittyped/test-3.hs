d4d4d00d26e4d413b6a1985e2900983523986c18
Takayuki Muranushi <muranushi@gmail.com>
2012-11-27 11:02:36 +0900

    annotate with the results.

--- unittyped/test-3.hs
+++ unittyped/test-3.hs
@@ -10,8 +10,14 @@ beta  =  1 gram * 1 meter * 1 second / 1 meter / 1 gram
 gamma =  1 second
 
 main = do
-  print $ alpha
-  print $ beta
-  print $ gamma
-  print $ gamma + beta
-  print $ beta  + gamma
+  print $ alpha         -- 1.0 g⋅m      
+  print $ beta          -- 1.0 g⋅m⋅s/m/g
+  print $ gamma         -- 1.0 s        
+  print $ gamma + beta  -- 2.0 s        
+  print $ beta  + gamma -- 2.0 g⋅m⋅s/m/g
+
+
+
+
+
+
